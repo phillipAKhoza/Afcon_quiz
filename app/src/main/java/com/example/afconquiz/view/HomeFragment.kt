a03da13ce5149fded0d25c7fee20dfe72bb64d2d
phillipAKhoza <phillipantonk@gmail.com>
2024-02-17 16:42:11 +0200

    add FragmentHomeBinding

--- app/src/main/java/com/example/afconquiz/view/HomeFragment.kt
+++ app/src/main/java/com/example/afconquiz/view/HomeFragment.kt
@@ -5,17 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.example.afconquiz.R
+import com.example.afconquiz.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-
+    lateinit var fmBinding : FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        fmBinding = FragmentHomeBinding.inflate(inflater,container,false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return fmBinding.root
     }
 
 }
