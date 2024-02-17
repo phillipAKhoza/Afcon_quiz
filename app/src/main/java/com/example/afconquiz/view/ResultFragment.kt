70b4d14dc92dcbb67f0f95fc8d887cff52e0d386
phillipAKhoza <phillipantonk@gmail.com>
2024-02-17 19:10:35 +0200

    add click listeners

--- app/src/main/java/com/example/afconquiz/view/ResultFragment.kt
+++ app/src/main/java/com/example/afconquiz/view/ResultFragment.kt
@@ -6,15 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.afconquiz.R
+import com.example.afconquiz.databinding.FragmentQuizBinding
+import com.example.afconquiz.databinding.FragmentResultBinding
 
 class ResultFragment : Fragment() {
-
+    lateinit var fmResultBinding : FragmentResultBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_result, container, false)
+        fmResultBinding = FragmentResultBinding.inflate(inflater,container,false)
+
+        fmResultBinding.btnNewQuiz.setOnClickListener {  }
+        fmResultBinding.btnExit.setOnClickListener {  }
+        return fmResultBinding.root
     }
 
 }
