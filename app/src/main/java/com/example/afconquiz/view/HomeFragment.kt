c1641367774f099631d6e9191f2d3132e981e97a
phillipAKhoza <phillipantonk@gmail.com>
2024-02-21 18:51:30 +0200

    call function on oncreateview

--- app/src/main/java/com/example/afconquiz/view/HomeFragment.kt
+++ app/src/main/java/com/example/afconquiz/view/HomeFragment.kt
@@ -19,7 +19,7 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         fmBinding = FragmentHomeBinding.inflate(inflater,container,false)
-
+        createAndOpenDB()
         fmBinding.btnBegin.setOnClickListener {
 
         }
