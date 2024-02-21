077e2b3b295f26e2ecb31d2f378e0f3814912d18
phillipAKhoza <phillipantonk@gmail.com>
2024-02-21 17:50:21 +0200

    db cursor rawQuery statement

--- app/src/main/java/com/example/afconquiz/database/FlagsDao.kt
+++ app/src/main/java/com/example/afconquiz/database/FlagsDao.kt
@@ -1,5 +1,6 @@
 package com.example.afconquiz.database
 
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import com.example.afconquiz.model.FlagsModel
 
@@ -7,5 +8,6 @@ class FlagsDao {
     fun getTenRandomQuestion(helper: DatabaseCopyHelper) : Array<FlagsModel>{
         val recordList : ArrayList<FlagsModel>
         val database : SQLiteDatabase = helper.writableDatabase
+        val cursor : Cursor = database.rawQuery("SELECT * FROM flags ORDER BY RANDOM() LIMIT 10", null)
     }
 }
