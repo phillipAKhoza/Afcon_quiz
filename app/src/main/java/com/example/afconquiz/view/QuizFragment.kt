2128722111e5ee895e6c9b2c9a78ccf882e58e6a
phillipAKhoza <phillipantonk@gmail.com>
2024-02-21 19:39:51 +0200

    add dao to get data and save in a array list

--- app/src/main/java/com/example/afconquiz/view/QuizFragment.kt
+++ app/src/main/java/com/example/afconquiz/view/QuizFragment.kt
@@ -6,17 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.afconquiz.R
+import com.example.afconquiz.database.DatabaseCopyHelper
+import com.example.afconquiz.database.FlagsDao
 import com.example.afconquiz.databinding.FragmentQuizBinding
+import com.example.afconquiz.model.FlagsModel
 
 
 class QuizFragment : Fragment() {
     lateinit var fmQuizBinding: FragmentQuizBinding
+    var flagList = ArrayList<FlagsModel>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
         fmQuizBinding = FragmentQuizBinding.inflate(inflater,container,false)
+
+        val dao = FlagsDao()
+        flagList = dao.getTenRandomQuestion(DatabaseCopyHelper(requireActivity()))
 
         fmQuizBinding.buttonA.setOnClickListener {  }
         fmQuizBinding.buttonB.setOnClickListener {  }
