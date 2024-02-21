6d2a0cd90ef32498a624530b95df8e75b545d727
phillipAKhoza <phillipantonk@gmail.com>
2024-02-21 17:09:03 +0200

    remove unused imports DB copy helper

--- app/src/main/java/com/example/afconquiz/database/DatabaseCopyHelper.kt
+++ app/src/main/java/com/example/afconquiz/database/DatabaseCopyHelper.kt
@@ -5,7 +5,6 @@ import android.database.SQLException
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteException
 import android.database.sqlite.SQLiteOpenHelper
-import android.widget.Toast
 import java.io.FileOutputStream
 import java.io.IOException
 import java.io.OutputStream
