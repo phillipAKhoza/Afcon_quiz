b4ded7b9b3bb5082a4b6f6374c8fb4daf06319d7
phillipAKhoza <phillipantonk@gmail.com>
2024-02-21 17:48:21 +0200

    add a function to get random questions

--- app/src/main/java/com/example/afconquiz/database/FlagsDao.kt
+++ app/src/main/java/com/example/afconquiz/database/FlagsDao.kt
@@ -1,4 +1,11 @@
 package com.example.afconquiz.database
 
+import android.database.sqlite.SQLiteDatabase
+import com.example.afconquiz.model.FlagsModel
+
 class FlagsDao {
+    fun getTenRandomQuestion(helper: DatabaseCopyHelper) : Array<FlagsModel>{
+        val recordList : ArrayList<FlagsModel>
+        val database : SQLiteDatabase = helper.writableDatabase
+    }
 }
