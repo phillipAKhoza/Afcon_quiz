3a485ad7553b5e35d8f9f57a1a286186a2ade320
phillipAKhoza <phillipantonk@gmail.com>
2024-02-21 21:27:13 +0200

    handle correct answer and wrong answer

--- app/src/main/java/com/example/afconquiz/view/QuizFragment.kt
+++ app/src/main/java/com/example/afconquiz/view/QuizFragment.kt
@@ -1,12 +1,14 @@
 package com.example.afconquiz.view
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.example.afconquiz.R
 import com.example.afconquiz.database.DatabaseCopyHelper
 import com.example.afconquiz.database.FlagsDao
@@ -42,7 +44,9 @@ class QuizFragment : Fragment() {
         }
 
         showData()
-        fmQuizBinding.buttonA.setOnClickListener {  }
+        fmQuizBinding.buttonA.setOnClickListener {
+
+        }
         fmQuizBinding.buttonB.setOnClickListener {  }
         fmQuizBinding.buttonC.setOnClickListener {  }
         fmQuizBinding.buttonD.setOnClickListener {  }
@@ -84,6 +88,35 @@ class QuizFragment : Fragment() {
         fmQuizBinding.buttonB.text = options[1].countryName
         fmQuizBinding.buttonC.text = options[2].countryName
         fmQuizBinding.buttonD.text = options[3].countryName
+
+    }
+    private fun answerControl(button : Button){
+        val clickedButton = button.text.toString()
+        val correctAnswer = correctFlag.countryName
+
+        if(clickedButton == correctAnswer){
+            correctNumber++
+            fmQuizBinding.tvCorrect.text = correctNumber.toString()
+            button.setBackgroundColor(Color.GREEN)
+        }else{
+            wrongNumber++
+            fmQuizBinding.tvWrong.text = wrongNumber.toString()
+            button.setBackgroundColor(Color.RED)
+            button.setTextColor(Color.WHITE)
+
+            when(correctAnswer){
+                fmQuizBinding.buttonA.text -> fmQuizBinding.buttonA.setBackgroundColor(Color.GREEN)
+                fmQuizBinding.buttonB.text -> fmQuizBinding.buttonB.setBackgroundColor(Color.GREEN)
+                fmQuizBinding.buttonC.text -> fmQuizBinding.buttonC.setBackgroundColor(Color.GREEN)
+                fmQuizBinding.buttonD.text -> fmQuizBinding.buttonD.setBackgroundColor(Color.GREEN)
+
+            }
+        }
+
+        fmQuizBinding.buttonA.isClickable = false
+        fmQuizBinding.buttonB.isClickable = false
+        fmQuizBinding.buttonC.isClickable = false
+        fmQuizBinding.buttonD.isClickable = false
 
     }
 
