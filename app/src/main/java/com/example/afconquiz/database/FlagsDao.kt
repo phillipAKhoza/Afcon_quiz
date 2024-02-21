4c65ffe083a7ee05739774a2a360329afc881107
phillipAKhoza <phillipantonk@gmail.com>
2024-02-21 17:58:20 +0200

    get data and add it to the recordlist

--- app/src/main/java/com/example/afconquiz/database/FlagsDao.kt
+++ app/src/main/java/com/example/afconquiz/database/FlagsDao.kt
@@ -5,8 +5,8 @@ import android.database.sqlite.SQLiteDatabase
 import com.example.afconquiz.model.FlagsModel
 
 class FlagsDao {
-    fun getTenRandomQuestion(helper: DatabaseCopyHelper) : Array<FlagsModel>{
-        val recordList : ArrayList<FlagsModel>
+    fun getTenRandomQuestion(helper: DatabaseCopyHelper) : ArrayList<FlagsModel>{
+        val recordList = ArrayList<FlagsModel>()
         val database : SQLiteDatabase = helper.writableDatabase
         val cursor : Cursor = database.rawQuery("SELECT * FROM flags ORDER BY RANDOM() LIMIT 10", null)
 
@@ -14,6 +14,16 @@ class FlagsDao {
         val countryNameIndex = cursor.getColumnIndex("country_name")
         val flagNameIndex = cursor.getColumnIndex("flag_name")
 
+        while (cursor.moveToNext()){
 
+            val record = FlagsModel(
+                cursor.getInt(idIndex),
+                cursor.getString(countryNameIndex),
+                cursor.getString(flagNameIndex)
+            )
+            recordList.add(record)
+        }
+        cursor.close()
+        return  recordList
     }
 }
